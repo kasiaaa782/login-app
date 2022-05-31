27474bea7887af909a92e300cfc2abd52801f20b
Katarzyna Niemiec <katarzyna.niemiec@comarch.com>
2022-05-31 23:33:39 +0200

    Added cat cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { CardComponent } from './home/card/card.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, LoginComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
+  declarations: [AppComponent, HomeComponent, LoginComponent, CardComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 import { AuthenticationService } from '../core/service/authentication/authentication.service';
+
+interface CatInfo {
+  data: string;
+}
 
 @Component({
   selector: 'app-home',
@@ -8,11 +13,40 @@ import { AuthenticationService } from '../core/service/authentication/authentica
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  constructor(public authenticationService: AuthenticationService) {}
+  @HostListener('window:wheel') onWheel(e: Event): void {
+    this.getCatCardInfo();
 
-  ngOnInit(): void {}
+    console.log(window.pageYOffset);
+  }
+
+  cardsList: CatInfo[] = [];
+
+  constructor(
+    public authenticationService: AuthenticationService,
+    private http: HttpClient
+  ) {}
+
+  ngOnInit(): void {
+    this.getCatCardInfo();
+  }
 
   logout() {
     this.authenticationService.logout();
+  }
+
+  getCatCardInfo(): void {
+    const url = 'https://meowfacts.herokuapp.com/';
+
+    this.http.get<CatInfo>(url).subscribe((card) => {
+      let isDuplicate = false;
+      this.cardsList.forEach((value) => {
+        if (value.data[0].normalize() === card.data[0].normalize()) {
+          isDuplicate = true;
+        }
+      });
+      if (!isDuplicate) {
+        this.cardsList.push(card);
+      }
+    });
   }
 }
