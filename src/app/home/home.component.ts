4fa0e2ba35a020d35ad90609cd58b697e613aa4e
Katarzyna Niemiec <katarzyna.niemiec@comarch.com>
2022-06-04 21:03:07 +0200

    Added IntersectionObserver to get cards up to date

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,10 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { AuthenticationService } from '../core/service/authentication/authentication.service';
@@ -12,25 +18,44 @@ interface CatInfo {
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
-  @HostListener('window:wheel') onWheel(e: Event): void {
-    this.getCatCardInfo();
-
-    console.log(window.pageYOffset);
-  }
+export class HomeComponent implements OnInit, AfterViewInit {
+  @ViewChild('pagination') pagination: ElementRef;
 
   cardsList: CatInfo[] = [];
+
+  callback = () => {
+    this.getCatCardInfo();
+  };
+
+  observer = new IntersectionObserver(this.callback);
+
+  target: HTMLElement;
 
   constructor(
     public authenticationService: AuthenticationService,
     private http: HttpClient
   ) {}
 
+  ngAfterViewInit(): void {
+    if (globalThis.IntersectionObserver) {
+      this.target = <HTMLElement>this.pagination.nativeElement;
+      this.observer.observe(this.target);
+    }
+  }
+
   ngOnInit(): void {
+    this.getCatCardInfo();
+    this.getCatCardInfo();
     this.getCatCardInfo();
   }
 
-  logout() {
+  ngOnDestroy(): void {
+    if (globalThis.IntersectionObserver) {
+      this.observer.unobserve(this.target);
+    }
+  }
+
+  logout(): void {
     this.authenticationService.logout();
   }
 
@@ -38,15 +63,22 @@ export class HomeComponent implements OnInit {
     const url = 'https://meowfacts.herokuapp.com/';
 
     this.http.get<CatInfo>(url).subscribe((card) => {
-      let isDuplicate = false;
-      this.cardsList.forEach((value) => {
-        if (value.data[0].normalize() === card.data[0].normalize()) {
-          isDuplicate = true;
-        }
-      });
-      if (!isDuplicate) {
+      if (this.checkIsDuplicatedCard(card)) {
+        console.log('duplikat');
+        this.getCatCardInfo();
+      } else {
         this.cardsList.push(card);
       }
     });
+  }
+
+  private checkIsDuplicatedCard(card: CatInfo): boolean {
+    let isDuplicate = false;
+    this.cardsList.forEach((value) => {
+      if (value.data[0].normalize() === card.data[0].normalize()) {
+        isDuplicate = true;
+      }
+    });
+    return isDuplicate;
   }
 }
